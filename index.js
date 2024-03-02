33a10288429e23fc6613515f7be6dfde70c3f23a
Abdellah Edaoudi <154640886+AbdellahEdaoudi@users.noreply.github.com>
2024-03-02 19:07:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 200 || process.env.PORT;
 
 // CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ed-portfolio-six.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
